0de3ca223f97848e7030279895b3b20d62baaebc
goonnow <goonnow@rocketmail.com>
2014-03-05 16:49:00 +0700

    Add more log

--- perl/index.pl
+++ perl/index.pl
@@ -8,4 +8,5 @@ BEGIN { unshift @INC, "$FindBin::Bin/lib" }
 
 # Start command line interface for application
 require Mojolicious::Commands;
+warn "Current folder : $FindBin::Bin";
 Mojolicious::Commands->start_app('JudeBot');
