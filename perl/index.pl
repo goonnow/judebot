c63e6c6688790e6428d45f4a76f647d718e51105
Pattarawat Chormai <goonnow@rocketmail.com>
2014-03-10 00:47:30 +0700

    Debug controller

--- perl/index.pl
+++ perl/index.pl
@@ -8,8 +8,6 @@ use File::Spec;
 use lib join '/', File::Spec->splitdir(dirname(__FILE__)), 'lib';
 use lib join '/', File::Spec->splitdir(dirname(__FILE__)), '..', 'lib';
 
-warn File::Spec->splitdir(dirname(__FILE__));
-
 # Start command line interface for application
 require Mojolicious::Commands;
 Mojolicious::Commands->start_app('JudeBot');
