90a1b4c1d28bc3ff92b89db68cc5bea1c7a09424
Pattarawat Chormai <goonnow@rocketmail.com>
2014-05-02 11:10:48 +0700

    Fix typo

--- perl/lib/JudeBot/Model/DailyPhrase.pm
+++ perl/lib/JudeBot/Model/DailyPhrase.pm
@@ -84,7 +84,7 @@ sub _combine_data {
 
     # Check duplicate title
     if( !grep { $_->{title} eq $new_data->{title} } @{ $old_data } ){
-        push @{$old_data, $new_data;
+        push @{$old_data}, $new_data;
     }
 
     my $length = scalar @{ $old_data };
